a2a696eed4f622cdfa0a21386c6c481211e19fba
Eric-Vadim <71000010+Eric-Vadim@users.noreply.github.com>
2020-11-05 10:46:56 +0300

    Add files via upload

---
+++ Lab5/X.java
@@ -0,0 +1,14 @@
+public class X {
+    protected String x;
+
+    X(String _x){
+        x = _x;
+    }
+
+    @Override
+    public String toString() {
+        return "X{" +
+                "x='" + x + '\'' +
+                '}';
+    }
+}
