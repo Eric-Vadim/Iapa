3544356625dfeb5a4cf93b8e3086f671f464e9ee
Eric-Vadim <71000010+Eric-Vadim@users.noreply.github.com>
2020-12-11 14:47:45 +0300

    Add files via upload

---
+++ com.poo/lab8/geometry/Cub.java
@@ -0,0 +1,8 @@
+package oop.lab8.geometry;
+
+public class Cub extends Parallelepiped {
+
+  public Cub(float side) {
+    super(side, side, side);
+  }
+}
---
+++ com.poo/lab8/geometry/GeometricBody.java
@@ -0,0 +1,8 @@
+package oop.lab8.geometry;
+
+public interface GeometricBody {
+
+  float getSurface();
+
+  float getVolume();
+}
